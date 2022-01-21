6ea65731432e164682a91e9d6a31e174c907c37a
Tim Franssen <mail@timendus.com>
2022-01-21 12:55:26 +0100

    Meukjes

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
-import MidiParser from 'midi-parser-js';
-import midiFile from "!!binary-loader!./music.mid";
-
-const midiData = new Uint8Array(midiFile.length);
-for (let i = 0; i < midiFile.length; i++) {
-    midiData[i] = midiFile.charCodeAt(i);
-}
-
-const obj = MidiParser.parse(midiData);
-
-console.log(obj);
-
 document.addEventListener('DOMContentLoaded', () => {
 
-  const track = 7;
-  const yMin = Math.min(...obj.track[track].event.filter(e => e.type == 9).map(e => e.data[0]));
-  const yMax = Math.max(...obj.track[track].event.filter(e => e.type == 9).map(e => e.data[0]));
-  console.log({ yMin, yMax })
-  let xPos = 0;
-  for ( const event of obj.track[track].event ) {
-    if ( event.type == 9 ) {
-      // console.log({ note: event.data[0], time: event.deltaTime });
-      const yPos = window.innerHeight - (event.data[0] * (window.innerHeight / 127));
-      const elm = document.createElement('div');
-      elm.classList.add('note');
-      elm.style.top = `${yPos}px`;
-      elm.style.left = `${xPos += (event.deltaTime/5)}px`;
-      document.body.appendChild(elm);
-    }
-  }
+  const canvas = document.querySelector('canvas');
+  const context = canvas.getContext('2d');
+  const playerSprite = document.querySelector('#sprites > #player');
+
+  // canvas.width = 1000;
+  // canvas.height = 333;
+  context.drawImage(playerSprite, 0, 0, playerSprite.width, playerSprite.height);
 
 });
