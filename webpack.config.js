cae76b1016ee7c6846a35549226441cfeed8d0be
Tim Franssen <mail@timendus.com>
2022-01-21 11:52:00 +0100

    Initial checkin

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: 'index.js'
+  },
+  devServer: {
+    port: 9000,
+    static: {
+      directory: path.resolve(__dirname, 'docs')
+    }
+  }
+}
